c39119f48f0de026562d305c1dcebd31fb053e66
sirdashasan <sirdashasan@hotmail.com>
2024-09-06 14:57:08 +0300

    menuControllers eklendi

--- api/routes/menuRoutes.js
+++ api/routes/menuRoutes.js
@@ -2,15 +2,10 @@ const express = require("express");
 const Menu = require("../models/Menu");
 const router = express.Router();
 
+const menuController = require("../controllers/menuControllers");
+
 // get all menu items
 
-router.get("/", async (req, res) => {
-  try {
-    const menus = await Menu.find({});
-    res.status(200).json(menus);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
+router.get("/", menuController.getAllMenuItems);
 
 module.exports = router;
