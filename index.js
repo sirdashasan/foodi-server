4013d07707276aa87c0d142c059a8604e5ad526b
sirdashasan <sirdashasan@hotmail.com>
2024-09-06 14:38:33 +0300

    menuRoutes eklendi

--- index.js
+++ index.js
@@ -13,10 +13,14 @@ app.use(express.json());
 
 mongoose
   .connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@demo-foodi-client.izv9r.mongodb.net/?retryWrites=true&w=majority&appName=demo-foodi-client`
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@demo-foodi-client.izv9r.mongodb.net/demo-foodi-client?retryWrites=true&w=majority&appName=demo-foodi-client`
   )
   .then(console.log("MongoDB Connected Successfully!"))
   .catch((error) => console.log("Error connecting to MongoDB!", error));
+
+// import import routes here
+const menuRoutes = require("./api/routes/menuRoutes");
+app.use("/menu", menuRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
