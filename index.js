57fae21cc10e9ec45e412ef23b37403734c564e0
sirdashasan <sirdashasan@hotmail.com>
2024-09-06 10:28:13 +0300

    new repo first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 6001;
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// mongodb configuration using mongoose
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@demo-foodi-client.izv9r.mongodb.net/?retryWrites=true&w=majority&appName=demo-foodi-client`
+  )
+  .then(console.log("MongoDB Connected Successfully!"))
+  .catch((error) => console.log("Error connecting to MongoDB!", error));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
