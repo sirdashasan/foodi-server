529099aa9c67e0e95c7b150a015b1bc46f31adf2
sirdashasan <sirdashasan@hotmail.com>
2024-09-09 11:48:33 +0300

    create ve delete user eklendi

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const userController = require("../controllers/userControllers");
 
 router.get("/", userController.getAllUsers);
+router.post("/", userController.createUser);
+router.delete("/:id", userController.deleteUser);
 
 module.exports = router;
