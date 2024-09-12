a9032fc119b416a747ece23a9d3770fc6f3707ac
sirdashasan <sirdashasan@hotmail.com>
2024-09-12 13:57:45 +0300

    menuControllers güncellendi

--- api/controllers/menuControllers.js
+++ api/controllers/menuControllers.js
@@ -2,7 +2,7 @@ const Menu = require("../models/Menu");
 
 const getAllMenuItems = async (req, res) => {
   try {
-    const menus = await Menu.find({});
+    const menus = await Menu.find({}).sort({ createdAt: -1 });
     res.status(200).json(menus);
   } catch (error) {
     res.status(500).json({ message: error.message });
