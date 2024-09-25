ae32f9508c575768adcd44256ab1abf24a9711b2
sirdashasan <sirdashasan@hotmail.com>
2024-09-25 18:32:00 +0300

    get payment eklendi

--- api/routes/paymentRoutes.js
+++ api/routes/paymentRoutes.js
@@ -24,4 +24,20 @@ router.post("/", verifyToken, async (req, res) => {
   }
 });
 
+// get payment
+router.get("/", verifyToken, async (req, res) => {
+  const email = req.query.email;
+  const query = { email: email };
+  try {
+    const decodedEmail = req.decoded.email;
+    if (email !== decodedEmail) {
+      res.status(403).json({ message: "Forbidden Access" });
+    }
+    const result = await Payment.find(query).sort({ createdAt: -1 }).exec();
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+});
+
 module.exports = router;
