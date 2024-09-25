7bfcca71b0325f570f43289d851998534db83450
sirdashasan <sirdashasan@hotmail.com>
2024-09-25 12:59:16 +0300

    Stripe PaymentMethod eklendi

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 6001;
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 // middleware
 app.use(cors());
@@ -35,6 +36,26 @@ const userRoutes = require("./api/routes/userRoutes");
 app.use("/menu", menuRoutes);
 app.use("/carts", cartRoutes);
 app.use("/users", userRoutes);
+
+// stripe payment routes
+app.post("/create-payment-intent", async (req, res) => {
+  const { price } = req.body;
+  const amount = price * 100;
+
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: amount,
+    currency: "usd",
+
+    payment_method_types: ["card"],
+  });
+
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+    // [DEV]: For demo purposes only, you should avoid exposing the PaymentIntent ID in the client-side code.
+    dpmCheckerLink: `https://dashboard.stripe.com/settings/payment_methods/review?transaction_id=${paymentIntent.id}`,
+  });
+});
 
 app.get("/", (req, res) => {
   res.send("Hello Foodi Client Server!");
