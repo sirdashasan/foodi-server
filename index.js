1483281e50a324b8ddde2d27c0009e110ccfb802
sirdashasan <sirdashasan@hotmail.com>
2024-10-02 10:46:35 +0300

    railway cors güncellemesi

--- index.js
+++ index.js
@@ -8,7 +8,15 @@ require("dotenv").config();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 // middleware
-app.use(cors());
+//app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://foodi-client-app-hazel.vercel.app",
+      "https://foodi-server-production.up.railway.app",
+    ],
+  })
+);
 app.use(express.json());
 
 //mongodb configuration using mongoose
